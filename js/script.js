b09142639b9611a2d78ff0d14e97a07cefc0bf13
NadyaTm <68203141+NadyaTm@users.noreply.github.com>
2022-10-31 16:39:27 -0300

    Validación de formulario

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 /*FORMULARIO*/
+/*var textarea=document.getElementById("textarea").value;
+    if(textarea==""){
+        alert("Ingrese su mensaje antes de enviar");        
+    }
+    else{
+        submit();
+    }*/
+/*Validación Formulario Bootstrap*/
+
+/*// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation')
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation')*/
+// Loop over them and prevent submission
+/*Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+form.classList.add('was-validated')
+    }, false)
+  })
+})()*/
 
 
 /* Menú */
